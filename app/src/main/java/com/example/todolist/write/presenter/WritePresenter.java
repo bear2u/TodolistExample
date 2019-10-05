016048d2d19176693cc97a31e08b0be4c48576d1
kim <bear2u@naver.com>
2019-10-05 11:50:30 +0900

    add presenter

--- app/src/main/java/com/example/todolist/write/presenter/WritePresenter.java
+++ app/src/main/java/com/example/todolist/write/presenter/WritePresenter.java
@@ -1,4 +1,33 @@
 package com.example.todolist.write.presenter;
 
-public class WritePresenter {
+import com.example.todolist.base.BasePresenter;
+import com.example.todolist.base.BasePresenterImpl;
+import com.example.todolist.model.Item;
+import com.example.todolist.write.WriteContract;
+
+public class WritePresenter
+        extends BasePresenterImpl<WriteContract.View>
+        implements WriteContract.Presenter {
+    WriteContract.View view;
+
+    @Override
+    public void setView(WriteContract.View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void removeView() {
+        this.view = null;
+    }
+
+    @Override
+    public void save(Item item) {
+        //TODO Repository에 저장
+        view.saveDone();
+    }
+
+    @Override
+    public void saveDone() {
+        //TODO Repository에서 저장완료
+    }
 }
--- app/src/main/java/com/example/todolist/write/view/WriteActivity.java
+++ app/src/main/java/com/example/todolist/write/view/WriteActivity.java
@@ -3,14 +3,37 @@ package com.example.todolist.write.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.todolist.R;
+import com.example.todolist.write.WriteContract;
+import com.example.todolist.write.presenter.WritePresenter;
 
-public class WriteActivity extends AppCompatActivity {
+public class WriteActivity extends AppCompatActivity
+        implements WriteContract.View {
+
+    WriteContract.Presenter presenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        presenter = new WritePresenter();
+        presenter.setView(this);
+
+        findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.save(null);
+            }
+        });
+    }
+
+    @Override
+    public void saveDone() {
+        Toast.makeText(this, "Save Done", Toast.LENGTH_SHORT).show();
     }
 }
